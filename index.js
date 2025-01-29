185ccab1a15c1b91d1d945c9a0c8a7540a4fe3e1
root <root@troy.nfsroot.de>
2025-01-29 09:39:40 +0100

    x

--- index.js
+++ index.js
@@ -6,15 +6,25 @@ tado.login(process.argv[2], process.argv[3])
 .then(() => {
   tado.getMe()
   .then(async (resp) => {
-    var anyOffline = false;
-    var aOffline = [];
+    var anyOffline = true;
     for(var home of resp["homes"]) {
       try {
         var devices = await tado.getDevices(home["id"]);
         for(var device of devices) {
-          if (!device["connectionState"] || !device["connectionState"]["value"]) {
-            anyOffline = true;
-            aOffline.push(device["deviceType"] + " " + device["serialNo"]);
+          if (device["serialNo"] == process.argv[4]) {
+            if (device["batteryState"] != "NORMAL") {
+              console.log("Warning: battery " + process.argv[4])
+              process.exit(1);
+            }
+            if (device["connectionState"]) {
+              if (device["connectionState"]["value"]) {
+                console.log("OK: online")
+                process.exit(0);
+              } else if (new Date().getTime() - new Date(device["connectionState"]["timestamp"]).getTime() < 3600000) {
+                console.log("OK: online " + (new Date().getTime() - new Date(device["connectionState"]["timestamp"]).getTime()) + " ago")
+                process.exit(0);
+              }
+            }
           }
         }
       } catch(e) {
@@ -22,12 +32,8 @@ tado.login(process.argv[2], process.argv[3])
         process.exit(2);
       }
     }
-    if (anyOffline) {
-      console.log("Critical: " + aOffline.join(", "))
-      process.exit(2);
-    }
-    console.log("OK: All online")
-    process.exit(0);
+    console.log("Critical: " + process.argv[4])
+    process.exit(2);
   })
   .catch((e) => {
     console.log("Critical: getMe failed")
